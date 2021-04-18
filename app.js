cd0d1a27cab90412689fea5f4df4620e8b8d2362
kngjnssy <kingajanossy@gmail.com>
2021-04-18 19:06:46 +0200

    formatting

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const express = require('express')
+const SerialPort = require ("serialport")
+const mongoose = require('mongoose')
 
-const SerialPort = require ("serialport");
-const parsers = SerialPort.parsers;
-const parser = new parsers.Readline({ delimiter: '\r\n' });
+const parsers = SerialPort.parsers
+const parser = new parsers.Readline({ delimiter: '\r\n' })
+
+const app = express()
 
 // mongoose.connect(
 //     'mongodb+srv://kinga:' + 
@@ -23,44 +24,11 @@ var port = new SerialPort('/dev/tty.usbmodem1411',{
     parity: 'none',
     stopBits: 1,
     flowControl: false,
-});
+})
 
-port.pipe(parser);
+port.pipe(parser)
 parser.on('data', function(data){
     const now = Date.now();
     console.log(now, data)
-});
+})
 
-// const express = require('express');
-// const app = express();
-// const mongoose = require('mongoose');
-
-// const SerialPort = require ("serialport");
-// const parsers = SerialPort.parsers;
-// const parser = new parsers.Readline({ delimiter: '\r\n' });
-
-// // mongoose.connect(
-// //     'mongodb+srv://kinga:' + 
-// //         process.env.MONGO_ATLAS_PW + 
-// //         '@node-rest-shop.coay9.mongodb.net/node-rest-shop?retryWrites=true&w=majority',
-// //     {   
-// //         useNewUrlParser: true,
-// //         useUnifiedTopology: true  
-// //     },
-    
-// // )
-
-// var port = new SerialPort('/dev/tty.usbmodem1421',{
-//     baudRate: 9600,
-//     dataBits: 8,
-//     parity: 'none',
-//     stopBits: 1,
-//     flowControl: false,
-// });
-
-// port.pipe(parser);
-// parser.on('data', function(data){
-//     const now = Date.now();
-//     console.log(now, data)
-// });
-   
